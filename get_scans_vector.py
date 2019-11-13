24f79b0c155a0af1e63ad1f33ab1cb80a4515906
rtull <rot358@utulsa.edu>
2019-11-13 12:13:27 -0600

    added get_scans_vector file

---
+++ get_scans_vector.py
@@ -0,0 +1,15 @@
+import numpy as np
+import ray_tracing as ray
+
+
+def get_scans_vector(grid_map,n_angles,cell_size,x,y,th):
+
+    vector_scans = np.full([1, n_angles], np.nan)
+    angle_resolution = 360/n_angles
+
+    for i in range(n_angles):
+
+        [vector_scans[i], _, _] = ray.ray_tracing(x, y, th, grid_map, cell_size)
+        th = th + angle_resolution
+
+    return vector_scans
